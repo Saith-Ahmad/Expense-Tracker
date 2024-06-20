acea38c7e8e7f1b43fdb438fd216356490db8041
Saith-Ahmad <saitthahmedmaqsood@gmail.com>
2024-06-21 00:52:10 +0500

    2nd commit

--- frontend/src/pages/url.js
+++ frontend/src/pages/url.js
@@ -1,3 +1,4 @@
 // export const baseURL = "https://talented-ruby-suit.cyclic.app/api/v1";
 export const baseURL = "http://localhost:8080/api/v1";
+// export const baseURL = "https://expense-tracker-topaz-six.vercel.app/api/v1";
 // export const baseURL = "http://localhost:8000/api/v1";
